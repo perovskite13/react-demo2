e547a03d634e1e70cfd0a65feba73760a9d377c5
perovskite13 <mooyy94@gmail.com>
2020-09-12 12:28:14 +0800

    add dynamic css

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -7,17 +7,22 @@ import "./App.css";
 function App(){
   //variables //storing the information in variables
   const [todos, setTodos] = useState([
-    "Finish plus project",
-    "Feed peggy",
-    "Sleep",
+    {text: "Finish plus project", isCompleted: false}, //object and attribute
+    {text: "Feed peggy",isCompleted: false},
+    {text: "Sleep",isCompleted: false},
   ]);
 
   //methods
-  const addTodo= (text) => {
-    const newTodos = [...todos, text]; //unpacking existing default todos, then append new todo input to the new const
+  const addTodo= (text) => { //initially was a string, just turn to object with curly braces
+    const newTodos = [...todos, {text}]; //unpacking existing default todos, then append new todo input to the new const
     setTodos(newTodos); //update the state
   };
 
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true; //render selected todo [index] is completed
+    setTodos(newTodos);
+  }
 
   //template
   return (
@@ -26,7 +31,12 @@ function App(){
   <div className = "todo-list">
     <h1> My todo list</h1>
     {todos.map((todo, index) =>(
-      <TodoItem todo = {todo} key = {index}/> //loop through the variables and print them all out
+      <TodoItem 
+      todo = {todo} 
+      key = {index}
+      index = {index}
+      completeTodo = {completeTodo}
+      /> //loop through the variables and print them all out
     ))}
     <TodoForm addTodo = {addTodo}/>
   </div>
--- todo-list/src/components/TodoItem/TodoItem.jsx
+++ todo-list/src/components/TodoItem/TodoItem.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "./TodoItem.css";
 
-function Todo(props){
-    const { todo } = props;
-    return <div className = "todo"> {todo}</div>;
+function Todo(props){ //pass in individual todo index and whether complete or not info //return complete css format if true (trigger attribute to True); if false, return empty css string
+    const { todo, index, completeTodo } = props;
+    return (
+    <div className = {`todo ${todo.isCompleted ? "complete" : ""}`}> 
+        {todo.text}
+        <div>
+            <button onClick={() => completeTodo(index)}>Complete</button>
+        </div>
+    </div>
+    ); //pass in text attribute
 }
 
 export default Todo;
