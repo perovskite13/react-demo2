a1a6874c9df688a1617cd277e512372e15f68bc1
perovskite13 <mooyy94@gmail.com>
2020-09-20 18:36:05 +0800

    hook component

--- number-counter/src/App.js
+++ number-counter/src/App.js
@@ -1,79 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 //import ChangeNumber from "./components/ChangeNumber"; 
 import logo from './logo.svg';
 import './App.css';
 
-//app.js
-//import Form from "./components/Form";
+//HOOK FUNCTION DEMO:
+function App(){
+  //variables //storing the information in variables
+  const [number, setNumber] = useState(
+    0
+  );
 
-class App extends React.Component {
-  constructor() {
-      super()
-      this.state = {
-          count: 0
-      }
-      this.increment = this.increment.bind(this)
-      this.decrement = this.decrement.bind(this)
-
-
-  }
-  
-  increment() {
-    //this.setState({ count: 1 })
-    this.setState(prevState => {
-      return {
-          count: prevState.count + 1
-      }
-  })
-  }
-
-  decrement() {
-    //this.setState({ count: 1 })
-    this.setState(prevState => {
-      return {
-          count: prevState.count - 1
-      }
-  })
-  }
-  
-  render() {
-      return (
-          <div className = "App">
-            <div className = "App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h1> Cool Number Counter</h1>
-              <h2>Plus or Minus?:</h2>
-              <h1>{this.state.count}</h1>
-              <div >
-                <button className = "button" onClick={this.decrement}> - </button>
-                <p>  </p>
-                <button className = "button" onClick={this.increment}> + </button>
-              </div>
-              
-              </div>
-          </div>
-      )
-  }
+  return (
+    <div className = "App">
+      <div className = "App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1> Cool Number Counter</h1>
+        <h2>Plus or Minus?:</h2>
+        <h1>{number}</h1>
+        <div >
+          <button className = "button" onClick={() => setNumber(number-1)}> - </button>
+          <p>  </p>
+          <button className = "button" onClick={() => setNumber(number+1)}> + </button>
+        </div>
+        
+        </div>
+    </div>
+)
 }
+export default App;
 
-// function App(){
-//   //variables //storing the information in variables
-//   const [number, setNumber] = useState(
-//     0
-//   );
+//CLASS FUNCTION DEMO:
+// class App extends React.Component {
+//   constructor() {
+//       super()
+//       this.state = {
+//           count: 0
+//       }
+//       this.increment = this.increment.bind(this)
+//       this.decrement = this.decrement.bind(this)
+//   }
+  
+//   increment() {
+//     //this.setState({ count: 1 })
+//     this.setState(prevState => {
+//       return {
+//           count: prevState.count + 1
+//       }
+//   })
+//   }
 
+//   decrement() {
+//     //this.setState({ count: 1 })
+//     this.setState(prevState => {
+//       return {
+//           count: prevState.count - 1
+//       }
+//   })
+//   }
+  
+//   render() {
+//       return (
+//           <div className = "App">
+//             <div className = "App-header">
+//               <img src={logo} className="App-logo" alt="logo" />
+//               <h1> Cool Number Counter</h1>
+//               <h2>Plus or Minus?:</h2>
+//               <h1>{this.state.count}</h1>
+//               <div >
+//                 <button className = "button" onClick={this.decrement}> - </button>
+//                 <p>  </p>
+//                 <button className = "button" onClick={this.increment}> + </button>
+//               </div>
+              
+//               </div>
+//           </div>
+//       )
+//   }
+// }
 
-//   // function handleClick(){
-//   //   setNumber(number);
-//   // }
+  // function handleClick(){
+  //   setNumber(number);
+  // }
 
-//   // const change = (index) =>{
-//   //   console.log(index);
-//   //   const newNumber = [...number];
-//   //   setNumber(newNumber)
-//   // };
+  // const change = (index) =>{
+  //   console.log(index);
+  //   const newNumber = [...number];
+  //   setNumber(newNumber)
+  // };
 
-//   //template
+  //template
 //   return (
 //   <div className = "App"> 
 //   <div className = "App-header">
@@ -85,8 +100,7 @@ class App extends React.Component {
 //   </div>
 // </div>
 //   );
-// }
 
-export default App;
+
 
 
